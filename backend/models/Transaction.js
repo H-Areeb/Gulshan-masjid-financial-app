403b34d1fab4d485d86f7a259ef82bf9fbbbc154
H-Areeb <areeburrehman13@gmail.com>
2025-06-29 00:34:46 +0500

    models created

---
+++ backend/models/Transaction.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+  transactionId: { type: String, unique: true }, // 👈 NEW FIELD
+  date: { type: Date, required: true },
+  description: { type: String, required: true },
+  type: { type: String, enum: ['income', 'expense'], required: true },
+  amount: { type: Number, required: true },
+  category: { type: String, required: true },
+  attachmentUrl: { type: String },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Transaction', transactionSchema);
